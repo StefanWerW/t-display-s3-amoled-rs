4a7014d5bc5506bb2bc1931a944fc1fc3d0e21b9
Stefan Werwitzke <scaw.dev@gmail.com>
2026-04-24 02:19:12 +0200

    wip

--- src/bin/amoled-dma.rs
+++ src/bin/amoled-dma.rs
@@ -4,7 +4,6 @@
 extern crate alloc;
 
 use alloc::string::String;
-use hal::spi::master::Spi;
 use core::fmt::Write;
 use core::mem::MaybeUninit;
 use embedded_graphics::mono_font::ascii::FONT_10X20;
@@ -18,47 +17,41 @@ use hal::dma::DmaPriority;
 use hal::gdma::Gdma;
 use hal::gpio::NO_PIN;
 use hal::prelude::_fugit_RateExtU32;
+use hal::spi::master::prelude::*;
+use hal::spi::master::Spi;
 use hal::systimer::SystemTimer;
 use hal::{
-    clock::ClockControl, peripherals::Peripherals, prelude::*, timer::TimerGroup, Delay, Rtc,
-    IO,
+    clock::ClockControl, peripherals::Peripherals, prelude::*, timer::TimerGroup, Delay, Rtc, IO,
 };
-use hal::spi::master::prelude::*;
 use t_display_s3_amoled::rm67162::Orientation;
 #[global_allocator]
-
 #[unsafe(export_name = "esp_app_desc")]
 #[unsafe(link_section = ".rodata_desc")]
 #[used]
-pub static ESP_APP_DESC: esp_bootloader_esp_idf::EspAppDesc = esp_bootloader_esp_idf::EspAppDesc::new_internal(
-    env!("CARGO_PKG_VERSION"),
-    env!("CARGO_PKG_NAME"),
-    "00:00:00",
-    "2026-04-23",
-    "esp-hal",
-    0,
-    u16::MAX,
-    65536,
-    0,
-);
+pub static ESP_APP_DESC: esp_bootloader_esp_idf::EspAppDesc =
+    esp_bootloader_esp_idf::EspAppDesc::new_internal(
+        env!("CARGO_PKG_VERSION"),
+        env!("CARGO_PKG_NAME"),
+        "00:00:00",
+        "2026-04-23",
+        "esp-hal",
+        0,
+        u16::MAX,
+        65536,
+        0,
+    );
 
-fn init_heap() {    
+fn init_heap() {
     const HEAP_SIZE: usize = 32 * 1024;
 
-    unsafe {
-    }
+    unsafe {}
 }
 
 #[hal::main]
 fn main() -> ! {
-
     // Disable the RTC and TIMG watchdog timers
 
-
     // Set GPIO4 as an output, and set its state high initially.
-
-
-
 
     let sclk = io.pins.gpio47;
     let rst = io.pins.gpio17;
@@ -69,23 +62,19 @@ fn main() -> ! {
     let d2 = io.pins.gpio48;
     let d3 = io.pins.gpio5;
 
-
-
     let dma_channel = dma.channel0;
 
     // Descriptors should be sized as (BUFFERSIZE / 4092) * 3
     let mut descriptors = [0u32; 12];
     let spi = Spi::new_half_duplex(
         peripherals.SPI2, // use spi2 host
-        75_u32.MHz(), // max 75MHz
+        75_u32.MHz(),     // max 75MHz
         hal::spi::SpiMode::Mode0,
-        &clocks)
-        .with_pins(Some(sclk),Some(d0),Some(d1),Some(d2),Some(d3),NO_PIN);
+        &clocks,
+    )
+    .with_pins(Some(sclk), Some(d0), Some(d1), Some(d2), Some(d3), NO_PIN);
 
-    
-    display
-        .set_orientation(Orientation::LandscapeFlipped);
-
+    display.set_orientation(Orientation::LandscapeFlipped);
 
     Text::with_alignment(
         "Hello,\nRust World!",
@@ -109,7 +98,7 @@ fn main() -> ! {
             } else {
                 0.0
             }
-        )
+        );
         Text::with_alignment(
             &s,
             Point::new(100, 40),
@@ -120,7 +109,7 @@ fn main() -> ! {
                 .build(),
             Alignment::Center,
         )
-        .draw(&mut display)
+        .draw(&mut display);
         cnt += 1;
     }
 }
---
+++ src/bin/bevy.rs
@@ -0,0 +1,142 @@
+#![no_std]
+#![no_main]
+
+extern crate alloc;
+
+use alloc::string::String;
+use core::fmt::Write;
+use core::mem::MaybeUninit;
+use embedded_graphics::mono_font::ascii::FONT_10X20;
+use embedded_graphics::mono_font::{MonoTextStyle, MonoTextStyleBuilder};
+use embedded_graphics::pixelcolor::Rgb565;
+use embedded_graphics::prelude::*;
+use embedded_graphics::text::{Alignment, Text};
+use esp_backtrace as _;
+use esp_println::println;
+
+use hal::{
+    delay::Delay,
+    gpio::{Io, Level, Output, OutputConfig},
+    spi::master::{Config as SpiConfig, Spi},
+    spi::Mode,
+    time::Rate,
+};
+
+use bevy::prelude::*;
+
+#[global_allocator]
+static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
+
+#[unsafe(export_name = "esp_app_desc")]
+#[unsafe(link_section = ".rodata_desc")]
+#[used]
+pub static ESP_APP_DESC: esp_bootloader_esp_idf::EspAppDesc =
+    esp_bootloader_esp_idf::EspAppDesc::new_internal(
+        env!("CARGO_PKG_VERSION"),
+        env!("CARGO_PKG_NAME"),
+        "00:00:00",
+        "2026-04-23",
+        "esp-hal",
+        0,
+        u16::MAX,
+        65536,
+        0,
+    );
+
+fn init_heap() {
+    const HEAP_SIZE: usize = 32 * 1024;
+    static mut HEAP: MaybeUninit<[u8; HEAP_SIZE]> = MaybeUninit::uninit();
+
+    unsafe {
+        ALLOCATOR.init(HEAP.as_mut_ptr() as *mut u8, HEAP_SIZE);
+    }
+}
+
+#[hal::main]
+fn main() -> ! {
+    init_heap();
+    let peripherals = hal::init(hal::Config::default());
+    println!("Hello world!");
+
+    let mut delay = Delay::new();
+
+    let mut led = Output::new(peripherals.GPIO38, Level::High, OutputConfig::default());
+    println!("GPIO init OK");
+
+    let sclk = peripherals.GPIO47;
+    let mut rst = Output::new(peripherals.GPIO17, Level::High, OutputConfig::default());
+    let mut cs = Output::new(peripherals.GPIO6, Level::High, OutputConfig::default());
+
+    let d0 = peripherals.GPIO18; // MOSI
+    let d1 = peripherals.GPIO7; // DC
+
+    let dc = Output::new(d1, Level::High, OutputConfig::default());
+
+    let spi = Spi::new(
+        peripherals.SPI2,
+        SpiConfig::default()
+            .with_frequency(Rate::from_mhz(75))
+            .with_mode(Mode::_0),
+    )
+    .unwrap()
+    .with_sck(sclk)
+    .with_mosi(d0);
+
+    let mut display = t_display_s3_amoled::rm67162::RM67162::new(spi, cs, dc);
+    display.reset(&mut rst, &mut delay).unwrap();
+    println!("reset display");
+    display.init(&mut delay).unwrap();
+    display
+        .set_orientation(t_display_s3_amoled::rm67162::Orientation::LandscapeFlipped)
+        .unwrap();
+
+    println!("init display");
+
+    display.clear(Rgb565::WHITE).unwrap();
+    println!("screen init ok");
+
+    let character_style = MonoTextStyle::new(&FONT_10X20, Rgb565::RED);
+    Text::with_alignment(
+        "Hello,\nRust World!",
+        Point::new(300, 20),
+        character_style,
+        Alignment::Center,
+    )
+    .draw(&mut display)
+    .unwrap();
+
+    let mut cnt = 0;
+    let started = hal::time::Instant::now().duration_since_epoch().as_millis();
+
+    loop {
+        //display.clear(Rgb565::WHITE).unwrap();
+        // fps testing
+        let mut s = String::new();
+
+        let elapsed = hal::time::Instant::now().duration_since_epoch().as_millis() - started;
+        core::write!(
+            &mut s,
+            "Frames: {}\nFPS: {:.1}",
+            cnt,
+            if elapsed > 0 {
+                cnt as f32 / (elapsed as f32 / 1000.0)
+            } else {
+                0.0
+            }
+        )
+        .unwrap();
+        Text::with_alignment(
+            &s,
+            Point::new(100, 40),
+            MonoTextStyleBuilder::new()
+                .background_color(Rgb565::BLACK)
+                .text_color(Rgb565::CSS_BISQUE)
+                .font(&FONT_10X20)
+                .build(),
+            Alignment::Center,
+        )
+        .draw(&mut display)
+        .unwrap();
+        cnt += 1;
+    }
+}
